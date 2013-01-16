d980e6616dde7f1fa53f98c386ea862dfc973501
Brian Dalton <bdalton@tagworldwide.com>
2013-01-16 08:57:17 +0000

    - Updated error types  - Updated debugging system  - Fixed bug wherein hidden files were being ignored  - Added ability to ignore fatal errors  - Added ability to override runonce setting  - Fixed self-detection code

--- cs.pl
+++ cs.pl
@@ -15,6 +15,13 @@ use constant ERROR_SIZE_MISMATCH 	=> 4;
 use constant ERROR_EXISTANCE_MISMATCH => 8;
 use constant ERROR_INVALID_HANDLE 	=> 16;
 
+use constant TYPE_FATAL				=> -1;
+use constant TYPE_ERROR				=> 0;
+use constant TYPE_MESSAGE			=> 1;
+use constant TYPE_WARNING			=> 2;
+use constant TYPE_INFORMATION		=> 3;
+use constant TYPE_DEBUG				=> 4;
+
 
 my $ZIPPATH = "/usr/bin/zip";
 my $UNZIPPATH = "/usr/bin/unzip";
@@ -30,11 +37,13 @@ my $dirpattern = ".*";
 my $filepattern = ".*";
 
 #GENERAL OPTIONS
-my $debuglevel = 0;				#DEBUGGING VERBOSITY [-1 == SILENT, 2 == VERY VERBOSE]
-my $sleeplen = 5;				#LENGTH TO WAIT BETWEEN MAIN LOOPS
-my $stablesleeplen = 2;			#LENGTH TO WAIT BETWEEN FILESIZE COMPARISONS IN A GIVEN FOLDER
-my $harmless = "false";			#WHETHER OR NOT TO PROHIBIT FILE MODIFICATIONS
-my $cleanup = "true";			#WHETHER OR NOT TO DELETE EMPTY DIRECTORIES
+my $debuglevel = TYPE_WARNING;		#DEBUGGING VERBOSITY [TYPE_FATAL == SILENT, TYPE_WARNING=DEFAULT, TYPE_DEBUG == VERY VERBOSE]
+my $sleeplen = 5;					#LENGTH TO WAIT BETWEEN MAIN LOOPS
+my $stablesleeplen = 2;				#LENGTH TO WAIT BETWEEN FILESIZE COMPARISONS IN A GIVEN FOLDER
+my $harmless = "false";				#WHETHER OR NOT TO PROHIBIT FILE MODIFICATIONS
+my $cleanup = "true";				#WHETHER OR NOT TO DELETE EMPTY DIRECTORIES
+my $robust = "true";					#WHETHER OR NOT TO DIE ON FATAL ERRORS
+my $runonce = "true";				#WHETHER OR NOT WE ONLY EVER WANT ONE VERSION OF THE PROGRAM RUNNING AT A TIME
 
 
 my @curdirFileList;
@@ -56,77 +65,104 @@ my @curdirFileList;
 sub processRootDir
 {
 	my ($rootpath, $dirfilter, $filter) = @_;
+	#EXPAND ROOTPATH INTO AN ABSOLUTE PATHNAME
 	$rootpath = realpath($rootpath) . "/";
 	
-	opendir ROOTDIR, $rootpath or die "Couldn't opern dir '$rootpath': $!";
+	
+	opendir ROOTDIR, $rootpath or die "Couldn't open dir '$rootpath': $!";
+	debugPrint(TYPE_DEBUG, "Searching root dir with filter $filter");
+	
 	
 	#PROCESS EACH DIRECTORY FOUND IN ROOT
 	while(my $file = readdir(ROOTDIR))
 	{
-		if($file =~ /^\./) { next; }
-		if($file !~ m/$dirfilter/) { if($debuglevel > 1) { print ".......$file didn't pass $dirfilter; skipping\n"; } next; }
+		my $curdir = "$rootpath$file/";
 		
+		
+		if($file =~ /^\.{1,2}$/) { next; }
+		if($file !~ m/$dirfilter/) { debugPrint(TYPE_WARNING, "$file ignored by filter"); next; }
+		debugPrint(TYPE_DEBUG, "Checking directory $curdir");
+		debugPrint(TYPE_MESSAGE, "Working on $file");
+		
+		#PREPARE ZIP COMMAND ARGUMENTS TO BE USED WITH system()
 		my @commandList = ($ZIPPATH);
 		if($debuglevel < 1) { push(@commandList, "-q"); }
 		push(@commandList, "$temppath$file.zip");
-
+		
+		
 		#SKIP FILES IN THE ROOT DIRECTORY
-		my $curdir = "$rootpath$file/";
 		unless(-d $curdir) { next; }
+		
 		
 		#CAN WE ZIP THE DIRECTORY?
 		@curdirFileList = ();
-		if($debuglevel > 1) { print "......Running subdirectory check with filter $filter on $curdir\n"; }
+		debugPrint(TYPE_INFORMATION, "Running subdirectory check with filter $filter");
 		my $retval = checkSubDir($curdir, "", $filter);
 		
-		#DELETE EMPTY DIRECTORY IF WE'VE BEEN TOLD TO
-		if(($retval == WARNING_EMPTY_DIR) && ($cleanup eq "true"))
+		if($retval == ERROR_SUCCESS)
 		{
-			if($debuglevel > 0) { print "...Cleaning empty directory \"$curdir\" as per request.\n"; }
-			rmdir($curdir);
-		}
-		elsif($retval == ERROR_SUCCESS)
-		{
-			unless(@curdirFileList) { if($debuglevel > 0) { print "...No matching files found for \"$curdir\". Skipping directory.\n"; } next; }
+			#IF WE'VE GOT NO FILELIST, DELETE THE DIRECTORY AND MOVE ON
+			unless(@curdirFileList)
+			{
+				debugPrint(TYPE_WARNING, "No matching files found for $curdir; cleaning and skipping directory");
+				cleanDirs("$curdir", $filter);
+				next;
+			}
+			#ADD THE FILELIST TO THE LIST SENT TO THE COMPRESSION PROGRAM
 			push(@commandList, @curdirFileList);
 			
-			if($debuglevel > 0) { print "...Writing zip file $temppath$file.zip...\n"; }
-			if($debuglevel > 1) { print "......Command is: \"@commandList\"\n"; }
-			#DIRECTORY IS READY FOR ZIPPING###
+			
+			
+			#DIRECTORY IS READY FOR ZIPPING
 			chdir($curdir);
+			debugPrint(TYPE_INFORMATION, "Moving to working directory $file");
+			debugPrint(TYPE_DEBUG, "Resolved as: $curdir");
+			debugPrint(TYPE_DEBUG, "Command is: \"@commandList\"");
+			
+			
+			#ZIP THE FILES
 			unless(system(@commandList))
 			{
 				chdir("$scriptpath");
-				if($debuglevel > 0) { print "...Done.\nMoving zip file to output directory... \n"; }
+				debugPrint(TYPE_DEBUG, "Moving to working directory $scriptpath");
+				debugPrint(TYPE_INFORMATION, "Moving zip file to output directory.");
 				
-				if(move("$temppath$file.zip", "$outpath$file.zip"))
+				#MOVE THE ZIP FILE INTO THE OUTPUT DIRECTORY
+				unless(move("$temppath$file.zip", "$outpath$file.zip"))
 				{
-					if($debuglevel > 0) { print "...Done.\n"; }
+					debugPrint(TYPE_ERROR, "Failed copying $temppath$file.zip to $outpath$file.zip");
 				}
 				
+				#CLEAN EVERYTHING UP
 				if($harmless ne "true")
 				{
+					#DELETE ALL FILES ZIPPED
 					my $delfile;
 					foreach $delfile (@curdirFileList)
 					{
-						if(unlink("$curdir$delfile")) { if($debuglevel > 1) { print "......Deleted \"$curdir$delfile\"\n"; } }
-						else					 { if($debuglevel > 1) { print "......Couldn't delete \"$curdir$delfile\"\n"; }}
-						
-						#TRY TO DELETE THE DIRECTORY IF IT IS EMPTY
-						if(rmdir(dirname("$curdir$delfile"))) { if($debuglevel > 1) { print "......Deleted empty directory \"" . dirname("$curdir$delfile") . "\"\n"; }}
+						if(unlink("$curdir$delfile")) { debugPrint(TYPE_INFORMATION, "Deleted \"$curdir$delfile\""); }
+						else						      { debugPrint(TYPE_WARNING, "Couldn't delete \"$curdir$delfile\""); }
 					}
 					
+					#DELETE ALL DIRECTORIES
+					debugPrint(TYPE_MESSAGE, "Cleaning directory $file");
+					cleanDirs($curdir, $filter);
 				}
 			}
 			else
 			{
-				if($debuglevel > 0) { print "...ZIP COMMAND FAILED.\n"; }
+				debugPrint(TYPE_ERROR, "Zip command failed");
 			}
 			chdir("$scriptpath");
 		}
+		elsif($retval == WARNING_EMPTY_DIR)
+		{
+			debugPrint(TYPE_MESSAGE, "Cleaning empty directory $file");
+			cleanDirs($curdir, $filter);
+		}
 		else
 		{
-			if($debuglevel > 0) { print "...CheckSubDir failed.\n"; }
+			debugPrint(TYPE_ERROR, "Checksubdir failed");
 		}
 	}
 
@@ -158,21 +194,24 @@ sub checkSubDir
 	my %filesizes;
 	my @directories;
 	
+	debugPrint(TYPE_INFORMATION, "Checking subdirectory $dirpath");
+	
+	
 	#BUILD A SIZELIST
 	my $dirhandle;
-	opendir $dirhandle, "$rootpath$dirpath" or return ERROR_INVALID_HANDLE;
+	opendir $dirhandle, "$rootpath$dirpath" or debugPrint(TYPE_FATAL, "Couldn't open $rootpath$dirpath for filesize globbing");
 	while(my $file = readdir($dirhandle))
 	{
 		my $filepath = "$rootpath$dirpath$file";
 		
 		#IGNORE . DIRECTORIES OR FILES NOT CAUGHT IN OUR REGEX
-		if($file =~ /^\./) { next; }
+		if($file =~ /^\.{1,2}$/) { next; }
 		if($filter ne "") { if($filepath !~ m/$filter/) { next; } }
 		
 		#IF IT'S A DIRECTORY, SAVE IT FOR LATER
 		if(-d $filepath)
 		{
-			if($debuglevel > 1) { print "......Found directory \"$dirpath$file\"\n"; }
+			debugPrint(TYPE_DEBUG, "Pending check on directory $dirpath$file");
 			push(@directories, "$dirpath$file/");
 		}
 		#IF IT'S A FILE, ADD ITS ORIGINAL SIZE TO THE SIZELIST
@@ -180,7 +219,7 @@ sub checkSubDir
 		{
 			my $size = -s "$filepath";
 			$filesizes{$file}  = $size;
-			if($debuglevel > 1) { print "......\"$filepath\" holds size: $size\n"; }
+			debugPrint(TYPE_DEBUG, "\"$file\" is $size bytes");
 		}
 		
 	}
@@ -191,12 +230,12 @@ sub checkSubDir
 		#COMPARE THE SIZELIST
 		sleep($stablesleeplen);
 
-		opendir $dirhandle, "$rootpath$dirpath" or return ERROR_INVALID_HANDLE;
+		opendir $dirhandle, "$rootpath$dirpath" or debugPrint(TYPE_FATAL, "Couldn't open $rootpath$dirpath for filesize checks");
 		while(my $file = readdir($dirhandle))
 		{
 			my $filepath = "$rootpath$dirpath$file";
 		
-			if($file =~ /^\./) { next; }
+			if($file =~ /^\.{1,2}$/) { next; }
 			if($filepattern ne "") { if($file !~ m/$filter/) { next; } }
 		
 			if(-d $filepath) {}
@@ -205,58 +244,111 @@ sub checkSubDir
 				#QUIT IF THE FILE HAS BEEN CREATED OR RESIZED SINCE WE CHECKED LAST
 				if(!exists($filesizes{$file}))
 				{
-					if($debuglevel > 1) { print "......\"$filepath\" has been created since check\n"; }
+					debugPrint(TYPE_WARNING, "\"$filepath\" has been created since last check");
+					closedir $dirhandle;
 					return ERROR_EXISTANCE_MISMATCH;
 				}
-		
-				if($filesizes{$file} != -s "$filepath")
+				
+				
+				my $val = -s "$filepath";;			#BODGE TO FIX SYNTAX HIGHLIGHTING ERROR WITH -s
+				if($filesizes{$file} != $val)
 				{
-					if($debuglevel > 1) { print "......\"$filepath\" fails filesize stability test\n"; }
+					debugPrint(TYPE_WARNING, "\"$filepath\" has a different filesize");
+					closedir $dirhandle;
 					return ERROR_SIZE_MISMATCH;
 				}
-			
+				
+				debugPrint(TYPE_DEBUG, "$dirpath$file passed filetest");
 				push(@curdirFileList, "$dirpath$file");
 			}
 		}
 		closedir $dirhandle;
 	}
 	
-	if(@directories)
+	
+	#IF WE MADE IT HERE, THE FILES IN THIS DIRECTORY ARE RELATIVELY STABLE (READ: PROBABLY FINISHED UPLOADING)
+	#CHECK ON SUBDIRECTORIES IF WE HAVE THEM
+	my $dir;
+	foreach $dir (@directories)
 	{
-		#IF WE MADE IT HERE, THE FILES IN THIS DIRECTORY ARE RELATIVELY STABLE (READ: PROBABLY FINISHED UPLOADING)
-		#CHECK ON SUBDIRECTORIES IF WE HAVE THEM
-		my $dir;
-		foreach $dir (@directories)
-		{
-			#PASS A 1 UP THE CHAIN IF FOUND
-			if($debuglevel > 1) { print "......Checking subdir $rootpath --> $dir\n"; }
-			if(my $retval = checkSubDir($rootpath, $dir, $filter))
-			{
-				#DELETE EMPTY DIRECTORIES IF WE'VE BEEN TOLD TO
-				if(($retval == WARNING_EMPTY_DIR) && ($cleanup eq "true"))
-				{
-					if($debuglevel > 0) { print "...Cleaning empty directory \"$rootpath$dir\" as per request.\n"; }
-					rmdir("$rootpath$dir");
-					delete $directories[$dir];
-				}
-				else
-				{
-					return $retval;
-				}
-			}
-		}
+		#PASS UP THE CHAIN IF FOUND
+		my $retval = checkSubDir($rootpath, $dir, $filter);
+		unless (($retval == WARNING_EMPTY_DIR) || ($retval == ERROR_SUCCESS)) { return $retval; }
 	}
 	
 	#IF THIS DIRECTORY IS EMPTY
-	if(!@directories && !%filesizes)
-	{
-		return WARNING_EMPTY_DIR;
-	}
+	if(!@directories && !%filesizes) { return WARNING_EMPTY_DIR; }
+	
 	
 	return ERROR_SUCCESS;
 }
 
 
+
+sub cleanDirs
+{
+	my ($dir, $filter) = @_;
+	
+	#BUILD A SIZELIST
+	my $dirhandle;
+	opendir $dirhandle, "$dir" or debugPrint(TYPE_FATAL, "Couldn't open directory $dir for cleaning");
+	
+	debugPrint(TYPE_DEBUG, "Directory is $dir");
+	
+	while(my $file = readdir($dirhandle))
+	{
+		#IGNORE . DIRECTORIES OR FILES NOT CAUGHT IN OUR REGEX
+		if($file =~ /^\.{1,2}$/) { next; }
+		if($filter ne "") { if("$dir$file" !~ m/$filter/) { next; } }
+		
+		if(-d "$dir$file")
+		{
+			cleanDirs("$dir$file/", $filter);
+		}
+	}
+	close $dirhandle;
+	
+	debugPrint(TYPE_DEBUG, "Deleting root folder [$dir]");
+	unless(rmdir("$dir"))
+	{
+		debugPrint(TYPE_ERROR, "Couldn't delete root folder $dir");
+	}
+}
+
+
+
+
+
+
+
+
+
+sub debugPrint
+{
+	
+	my ($level, $message) = @_;
+	
+	if($level == TYPE_FATAL)
+	{
+		print("\n#####################################\n");
+		print("## FATAL ERROR: $message [$!]\n");
+		if($robust eq "true") { print("## (Robust enabled; continuing regardless)\n"); }
+		print("#####################################\n\n");
+		if($robust eq "false") { die; } else { return; }
+	}
+	
+	unless($level <= $debuglevel) { return; };
+	
+	switch($level)
+	{
+	case [TYPE_INFORMATION] { print "Information:\t\t"; }
+	case [TYPE_MESSAGE] { print "Message:\t\t"; }
+	case [TYPE_WARNING] { print "WARNING:\t\t"; }
+	case [TYPE_ERROR] { print "***ERROR:\t\t"; }
+	case [TYPE_DEBUG] { print "-debug- "; }
+	}
+	print "$message\n";
+}
 
 
 
@@ -309,16 +401,18 @@ sub main
 				print "\t-a=TIME, --stabledelay=TIME\n";
 				print "\t\tSet the delay between checking filesizes within each folder, in seconds.\n";
 				print "\t\tDEFAULTS TO 2\n\n";
+				print "\t-S, --strict\n";
+				print "\t\tFail on fatal errors, rather than printing them and trying to move on.\n";
 				
 				exit;
 			}
 			case ["-v", "--verbose"]
 			{
-				$debuglevel = 1;
+				$debuglevel = TYPE_INFORMATION;
 			}
 			case ["-vv"]
 			{
-				$debuglevel = 2;
+				$debuglevel = TYPE_DEBUG;
 			}
 			case ["-q", "--quiet"]
 			{
@@ -362,6 +456,14 @@ sub main
 			{
 				$stablesleeplen = $parameter;
 			}
+			case ["-S", "--strict"]
+			{
+				$robust = "false";
+			}
+			case ["-m", "--multiple-instances"]
+			{
+				$runonce = "false";
+			}
 			default:
 			{
 			}
@@ -372,6 +474,11 @@ sub main
 
 	##APPEND TRAILING SLASH ONTO DIRECTORIES WHERE NEEDED
 	$scriptpath = getcwd();
+	
+	$inpath = realpath($inpath);
+	$temppath = realpath($temppath);
+	$outpath = realpath($outpath);
+	
 	if($inpath !~ m/.*\/$/) { $inpath .= "/"; }
 	if($temppath !~ m/.*\/$/) { $temppath .= "/"; }
 	if($outpath !~ m/.*\/$/) { $outpath .= "/"; }
@@ -381,33 +488,45 @@ sub main
 	
 	if($debuglevel >= 0)
 	{
-		print "Input path: $inpath\n";
-		print "Temporary path: $temppath\n";
-		print "Output path: $outpath\n";
-		if($debuglevel > 0)
-		{
-			print "...Pause between loops: $sleeplen\n";
-			print "...Pause between filesize checks: $stablesleeplen\n";
-		}
-		if($harmless eq "true")	{ print "Running in harmless mode.\n"; }
-		else			  			{ print "Running in dangerous mode (deleting processed directories).\n"; }
+		debugPrint(TYPE_MESSAGE, "Using input path:\t $inpath");
+		debugPrint(TYPE_MESSAGE, "Using working path:\t$temppath");
+		debugPrint(TYPE_MESSAGE, "Using output path:\t$outpath");
+		debugPrint(TYPE_INFORMATION, "Pause between loops: $sleeplen" . ($sleeplen == 0 ? " (Looping disabled)" : ""));
+		debugPrint(TYPE_INFORMATION, "Pause between filesize checks: $stablesleeplen");
+		
+		if($harmless eq "true") { debugPrint(TYPE_MESSAGE, "Running in harmless mode"); }
+		else						{ debugPrint(TYPE_WARNING, "Running in dangerous mode (will delete files/directories)"); }
 	}
 
 	open(SELF, "<", $0) or die "Cannot open $0 -- $!";
-	flock(SELF, LOCK_EX | LOCK_NB) or die "Already running";
+	unless(flock(SELF, LOCK_EX | LOCK_NB))
+	{
+		debugPrint(TYPE_FATAL, "Already running");
+		#THIS IS A SPECIAL CASE OF FATAL ERROR; DIE MANUALLY UNLESS WE REALLY DON'T WANT TO
+		if($runonce eq "true")
+		{
+			$robust = "false";
+			debugPrint(TYPE_FATAL, "NOT RUN WITH MULTIPLEINSTANCE PRIVILEGES (-m); QUITTING REGARDLESS OF ROBUST SETTINGS");
+			die;
+		}
+	}
 	
+	debugPrint(TYPE_MESSAGE, "EXECUTION STARTED");
 	while(1)
 	{
-		if($debuglevel >= 0) { print "CHECKING DIRECTORIES...\n"; }
+		debugPrint(TYPE_DEBUG, "Checking directories");
 		
 		processRootDir($inpath, $dirpattern, $filepattern);
 		
-		if($sleeplen == 0) { return; }
+		if($sleeplen == 0) { last; }
 		sleep($sleeplen);
 	}
+	
+	debugPrint(TYPE_MESSAGE, "EXECUTION FINISHED");
 
 	close(SELF);
 }
+
 
 
 main();
